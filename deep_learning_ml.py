c1e373ea913ecf4af7ff485dc9e0b28a71fd04b5
Homit Dalia <homitdalia59@gmail.com>
2023-11-04 07:22:53 +0530

    Update deep_learning_ml.py

--- deep_learning_ml.py
+++ deep_learning_ml.py
@@ -51,3 +51,58 @@ class_report = classification_report(y_test, y_pred_classes, target_names=label_
 
 print("Classification Report:")
 print(class_report)
+
+
+
+####################################################
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import classification_report
+
+# Load the CSV dataset
+data = pd.read_csv('iris.csv')
+
+# Split the dataset into features (X) and target labels (y)
+X = data[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']]
+y = data['species']
+
+# Encode the target labels into numerical values
+label_encoder = LabelEncoder()
+y = label_encoder.fit_transform(y)
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a deep learning model
+model = keras.Sequential([
+    keras.layers.Dense(16, activation='relu', input_shape=(4,)),
+    keras.layers.Dense(8, activation='relu'),
+    keras.layers.Dense(3, activation='softmax')
+])
+
+# Compile the model
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+model.fit(X_train, y_train, epochs=100, batch_size=32, validation_split=0.2)
+
+# Evaluate the model on the test data
+test_loss, test_accuracy = model.evaluate(X_test, y_test)
+print("Test Accuracy:", test_accuracy)
+
+# Make predictions on the test data
+y_pred = model.predict(X_test)
+y_pred_classes = [np.argmax(pred) for pred in y_pred]
+
+# Decode the numerical predictions back to labels
+y_pred_labels = label_encoder.inverse_transform(y_pred_classes)
+
+# Generate a classification report
+class_report = classification_report(y_test, y_pred_labels)
+print("Classification Report:")
+print(class_report)
